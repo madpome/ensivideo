03642c5f7c5b75aad1ad4fed215bd88af10e6816
Philippe EAR <earphilippefr@gmail.com>
2019-11-26 16:30:08 +0100

    init

---
+++ build/CMakeFiles/ensivideo.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/src/ensitheora.c" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/build/CMakeFiles/ensivideo.dir/src/ensitheora.c.o"
+  "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/src/ensivideo.c" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/build/CMakeFiles/ensivideo.dir/src/ensivideo.c.o"
+  "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/src/ensivorbis.c" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/build/CMakeFiles/ensivideo.dir/src/ensivorbis.c.o"
+  "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/src/oggstream.c" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/build/CMakeFiles/ensivideo.dir/src/oggstream.c.o"
+  "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/src/stream_common.c" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/build/CMakeFiles/ensivideo.dir/src/stream_common.c.o"
+  "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/src/synchro.c" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/build/CMakeFiles/ensivideo.dir/src/synchro.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/pome/ENSIMAG/2A/SEPC/ensimag-video
+# Build directory: /home/pome/ENSIMAG/2A/SEPC/ensimag-video/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(CommonPthreadError "perl" "/home/pome/ENSIMAG/2A/SEPC/ensimag-video/tests/pthcocci.pl")
